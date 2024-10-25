53d355379da5e8a59625ad9933c585735e443f5d
eoghan523 <eoghan523@gmail.com>
2024-10-25 13:25:08 +0100

    save changes to assigment week6 s2

--- W6-S2-datastructures-and-strings/3_Python-KeyDataStructures-P2/B_PythonDictionaries/assignment/eoghan_W6S2_3_B_assignment.py
+++ W6-S2-datastructures-and-strings/3_Python-KeyDataStructures-P2/B_PythonDictionaries/assignment/eoghan_W6S2_3_B_assignment.py
@@ -14,7 +14,7 @@ employee_id = 0 #Defines 'employee_id' variable and the value is 0
 
 for employee in my_dict["Employee"]:         #For loop that targets "Employee" values.
     employee['id'] = employee_id                   #adds an id value to employee values.
-    employee["retirement_eligible"] = employee["age"] >= retirement_age     #Adds a conditiional check on the age value anginst 'retirement_age' integer if its higher than or equal to '65'.   
+    employee["retirement_eligible"] = employee["age"] >= retirement_age     #Adds a conditiional check on the age value anginst 'retirement_age' integer if its higher than or equal to '65'. The output on the dictionary 
     employee_id += 1                                      #The for loop adds 1 to each employee id. 
 
 print(employee_id) #prints the 'employee_id' count.
@@ -23,7 +23,7 @@ print(employee_id) #prints the 'employee_id' count.
 new_hires = {
     "Employee" : [
          {"name": "Elenor", "age": 40, "email": "elenor@bloggs.com", "job": "Marketing"},
-         {"name": "Roger", "age": 63, "email": "roger@bloggs.com", "job": "Engineering"},
+         {"name": "Roger", "age": 64, "email": "roger@bloggs.com", "job": "Engineering"},
          {"name": "Steve", "age": 41, "email": "steve@bloggs.com", "job": "Catering"}
     ]
 }
@@ -37,19 +37,18 @@ for new_employee in new_hires["Employee"]:
 #This line of code merges the dictionaries from the 'my_dict' and 'new_hires'
 updated_list = {"Employees": my_dict["Employee"] + new_hires["Employee"]}
 
-print(updated_list)
+print(updated_list) #prints the updated list with employee id's in list 0 to 5
 
 
-print("Below is the list of employees retirement Eligible:")
-for employee in updated_list ["Employees"]:
-      if employee["retirement_eligible"]:
-            print(f'name: {employee["name"]}, {employee["age"]}, {employee["email"]}, {employee["job"]}, {employee["id"]}')
-        elif employee["age"] >= 63:
-            print(f'Near Retirement: {employee["name"]}, {employee["age"]}, {employee["email"]}, {employee["job"]}, {employee["id"]}')
-
-      
+print("Below is the list of employees retirement Eligible:") #Heading for the retirement age employees
+for employee in updated_list["Employees"]:    # checks the employees in the updated_list
+    if employee["retirement_eligible"]:       # If statement that catches retirement_eligible employees defined on lines 17 and 34
+        print(f' {employee["name"]}, age: {employee["age"]}, email: {employee["email"]}, job: {employee["job"]}, id: {employee["id"]}')   # The print statement uses a f string to display any employee that is greater than or equal to 65.
+    elif employee["age"] == 64:  # The elif catch catches employees near retirement that are greater than orr equal to 64
+        print(f'Near Retirement: {employee["name"]}, age: {employee["age"]}, email: {employee["email"]}, job: {employee["job"]}, id: {employee["id"]}') # The print statement uses a f string to display any employee that is greater than or equal to 64
 
 
+print()
 
 
 
