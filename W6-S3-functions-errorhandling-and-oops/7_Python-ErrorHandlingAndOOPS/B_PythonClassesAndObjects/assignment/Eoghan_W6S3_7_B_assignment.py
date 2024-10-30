6c96fc2f74b44f5f777a292e81b642f20581ce5b
eoghan523 <eoghan523@gmail.com>
2024-10-30 17:25:48 +0000

    progress save on daily assignment w6s3_7b

--- W6-S3-functions-errorhandling-and-oops/7_Python-ErrorHandlingAndOOPS/B_PythonClassesAndObjects/assignment/Eoghan_W6S3_7_B_assignment.py
+++ W6-S3-functions-errorhandling-and-oops/7_Python-ErrorHandlingAndOOPS/B_PythonClassesAndObjects/assignment/Eoghan_W6S3_7_B_assignment.py
@@ -12,4 +12,25 @@ def display_info(self):
     #prints f string function calling the obects under the car class 
     print(f"Car details: {self.make} {self.model}, year: {self.__year}")
 
-    
+#Function to update the cars year
+def update_year(self, new_year):
+        # Sets the private attribute __year to new_year
+        self.__year = new_year
+
+ # Function to get year
+def get_year(self):
+    # Returns the current year of the car
+    return self.__year
+
+ # Function to set the year attribute.
+def set_year(self, new_year):
+        # Updates the car's year to new_year
+        self.__year = new_year
+
+  #Function to delete car objects
+def __del__(self):
+        # Prints a message indicating the car object is being deleted
+        print(f"The car {self.make} {self.model} has been deleted.")
+
+
+
